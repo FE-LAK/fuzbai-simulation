6be9a9414303a43d82bb08441620e12ba2c425d8
David Hozic <davidhozic@gmail.com>
2025-12-15 02:25:00 +0100

    Update MuJoCo-rs

--- simulation/examples/stepping.rs
+++ simulation/examples/stepping.rs
@@ -1,44 +1,31 @@
-use fuzbai_simulator::{FuzbAISimulator, PlayerTeam, VisualConfig, render_viewer};
-use std::time::Instant;
-
+use fuzbai_simulator::{FuzbAISimulator, PlayerTeam, ViewerProxy, VisualConfig};
 
 
 fn main() {
     let mut sim = FuzbAISimulator::new(
-        10, 0, true, 0.0,
+        10, 0,
+        true,
+        0.055,
         None,
         VisualConfig::new(
             10, true,
-            0xFF,
-            None
+            0,
         )
     );
 
     sim.set_external_mode(PlayerTeam::RED,false);
     sim.set_external_mode(PlayerTeam::BLUE, true);
-    let mut t0 = Instant::now();
-    let mut n = 0usize;
 
-    sim.set_motor_command(vec![(2, 0.5, 0.0, 1.0, 1.0)], PlayerTeam::RED);
-    while sim.viewer_running() {
-        if sim.terminated() || sim.truncated() {
-            sim.reset_simulation();
+    let sim_thread = std::thread::spawn(move || {
+        while sim.viewer_running() {
+            if sim.terminated() || sim.truncated() {
+                sim.reset_simulation();
+            }
+            sim.step_simulation();
         }
-        sim.step_simulation();
-        render_viewer();
+    });
 
-        let mut xyz: [f64; 3] = sim.ball_true_state()[..3].try_into().unwrap();
-        xyz[2] = 0.0;
-
-        if sim.viewer_running() {
-            sim.show_estimates(Some(xyz), None);
-        }
-
-        n += 1;
-        if t0.elapsed().as_micros() > 1_000_000 {
-            println!("{n}");
-            t0 = Instant::now();
-            n = 0;
-        }
-    }
+    let viewer = ViewerProxy;
+    viewer.render_loop();
+    sim_thread.join().unwrap();
 }
