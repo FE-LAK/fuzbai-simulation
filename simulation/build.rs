7345234c6aa7e233b234928505eb848e9ddaa3d5
David Hozic <davidhozic@gmail.com>
2026-01-05 23:23:22 +0100

    Fix hole detector sizes and reduce build system deps

--- simulation/build.rs
+++ simulation/build.rs
@@ -1,35 +1,19 @@
-//! Build script for building
-extern crate mujoco_rs;
-use mujoco_rs::mujoco_c::*;
-use std::ffi::CString;
-use std::ptr;
-
+//! Build script for compiling an XML MuJoCo model into MJB.
+//! This is used for easier embedding inside the binary.
 
 fn main() {
     println!("cargo::rerun-if-changed=./build.rs");  // This build script changed.
     println!("cargo::rerun-if-changed=../models/");  // XML files or it's dependencies changed.
 
     // Model compilation
-    compile_model();
-}
-
-
-/// Compile the MuJoCo's XML model into the binary format.
-pub fn compile_model() {
     const MODEL_PATH: &str = "../models/miza.xml";
     const OUTPUT_PATH: &str = "./src/miza.mjb";
 
-    let model_path = CString::new(MODEL_PATH).unwrap();
-    let output_path = CString::new(OUTPUT_PATH).unwrap();
-
-    unsafe {
-        let model = mj_loadXML(model_path.as_ptr(), ptr::null(), ptr::null_mut(), 0);
-
-        if model.is_null() {
-            panic!("could not load {MODEL_PATH}");
-        }
-
-        mj_saveModel(model, output_path.as_ptr(), ptr::null_mut(), 0);
-        mj_deleteModel(model);
-    }
+    // The compiler is part of the MuJoCo's sample programs in official MuJoCo builds.
+    let _ = std::fs::remove_file(OUTPUT_PATH);
+    let output = std::process::Command::new("../mujoco-3.3.7/bin/compile")
+        .arg(MODEL_PATH)
+        .arg(OUTPUT_PATH)
+        .output()
+        .expect("failed to compile MuJoCo model from XML to MJB.");
 }
